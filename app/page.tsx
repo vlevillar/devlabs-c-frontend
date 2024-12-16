e02eaa43f9a7c81556d911c64dac25fa6d0bdf1a
Valentino Villar <vles2345@gmail.com>
2024-12-16 18:08:49 -0300

    login test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import LoginButton from "./components/LoginButton";
+import LogOutButton from "./components/LogOutButton";
+import Profile from "./components/Profile";
 
 export default function IndexPage() {
-  return <div><LoginButton/></div>;
+  return <div>
+    <LoginButton/>
+    <LogOutButton/>
+    <Profile/>
+    </div>;
 }
 
 export const metadata: Metadata = {
