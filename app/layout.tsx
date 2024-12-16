0b26b2c99210fef2d2b6beb3f632a5161d4c8291
Valentino Villar <vles2345@gmail.com>
2024-12-16 18:11:16 -0300

    login test

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: Props) {
       domain="dev-yxc4j2q43r3bckhs.us.auth0.com"
       clientId="Ds4d9GuIy2mBKnPaOoO9a3l3JDlxwmtl"
       authorizationParams={{
-        //redirect_uri: "https://devlabs-c-frontend.vercel.app/",
+        redirect_uri: "https://devlabs-c-frontend.vercel.app/",
       }}
     >
       <StoreProvider>
